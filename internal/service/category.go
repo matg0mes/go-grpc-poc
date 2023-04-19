c53e2aee3b052280dc2dbc987abc8e9969ede14a
matg0mes <hotmus8500@gmail.com>
2023-04-19 09:24:33 -0300

    feat(category): finish service

---
+++ internal/service/category.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"context"
+
+	"github.com/matg0mes/go-grpc-poc/internal/database"
+	"github.com/matg0mes/go-grpc-poc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryResponse) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Description: category.Description,
+		Name:        category.Name,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
